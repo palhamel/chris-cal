5a43d6c36f81c41d8183d08b4b7a5a3448d8dc46
Pål Hamel <pal.hamel@gmail.com>
2020-10-25 19:17:56 +0100

    cleanup in code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// Create todays date in right format:
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -6,16 +6,16 @@ var yyyy = today.getFullYear();
 today = yyyy + '-' + mm + '-' + dd;
 
 const currentDay = today;
-console.log(currentDay)
+console.log('todays date is >>>> ', currentDay)
 
-// ------------------------------
-// Get HTML elements:
+// Get HTML elements by  Class as a HTML Collection / "array":
 const dateElement= document.getElementsByClassName('grid-item');
 // console.log('collection:', dateElement);
 
-// Loop thru the HTML collection and match the 'data-date'
+// Loop thru the HTML collection and match the 'data-date' in dataset:
+// + Add new Class to matched
 for (let item of dateElement) {
-  console.log('date:', item.dataset.date);
+console.log('date:', item.dataset.date);
   if (item.dataset.date == currentDay) {
     item.classList.add("today")
   } else if (item.dataset.date > currentDay) {
@@ -24,40 +24,3 @@ for (let item of dateElement) {
     item.classList.add("past")
   }
 }
-
-
-
-// const specifiedDate = dateElement[2].dataset.date;
-
-// en for loop på arrayen?
-
-// console.log(specifiedDate)
-
-
-
-
-
-// console.log('Im here')
-
-/*
-
-jquery som funkar:
-
-$(function () {
-  var date = new Date(),
-    currentDate =
-      date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
-  $(".grid-item").each(function () {
-    var specifiedDate = $(this).data("date");
-
-    if (specifiedDate == currentDate) {
-      $(this).addClass("today");
-    } else if (currentDate > specifiedDate) {
-      $(this).addClass("past");
-    } else {
-      $(this).addClass("future");
-    }
-  });
-});
-
-*/
