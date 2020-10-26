5243704c69e75339d4e5494fca6fdea8e0e74078
Pål Hamel <pal.hamel@gmail.com>
2020-10-26 17:20:22 +0100

    add css and js for not active class

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Create todays date in right format:
+// Create todays date in the right format:
 let today = new Date();
 const dd = String(today.getDate()).padStart(2, '0');
 const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -6,21 +6,21 @@ const yyyy = today.getFullYear();
 today = yyyy + '-' + mm + '-' + dd;
 
 const currentDay = today;
-console.log('todays date is >>>> ', currentDay)
+console.log('todays date is >> ', currentDay)
 
-// Get HTML elements by  Class as a HTML Collection / "array":
+// Get HTML elements by  Class - as a HTML Collection / "array":
 const dateElement= document.getElementsByClassName('grid-item');
 // console.log('collection:', dateElement);
 
 // Loop thru the HTML collection and match the 'data-date' in dataset:
 // + Add new Class to matched
 for (let item of dateElement) {
-console.log('date:', item.dataset.date);
   if (item.dataset.date == currentDay) {
     item.classList.add("today")
   } else if (item.dataset.date > currentDay) {
-    item.classList.add("future")
+    item.classList.add("future", "not-active")
   } else {
-    item.classList.add("past")
+    item.classList.add("past", "not-active")
   }
+  console.log('date:', item.dataset.date);
 }
